7ef37a6f919d7d3348ea72c333cc355fdf8ac63e
luopc1218 <luopc1218@outlook.com>
2020-09-08 17:39:43 +0800

    搭建首页基本结构

---
+++ vue/hp-forum/src/views/home/pages/index/components/Posts/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="home-index-posts">
+        帖子列表
+        <div v-for="item in postList" :key="item">
+            <div class="home-index-posts-post">Col</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home-index-posts",
+        data() {
+            return {
+                postList: 7
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .home-index-posts {
+        padding: .5rem;
+
+        .home-index-posts-post {
+            padding: .5rem;
+            margin: .5rem;
+            border: 1px solid #eee;
+            border-radius: 6px;
+            cursor: pointer;
+            transition: all .3s ease-in-out;
+        }
+
+        .home-index-posts-post:hover {
+            box-shadow: 2px 2px 2px rgba(0, 0, 0, .1)
+
+
+        }
+    }
+</style>
---
+++ vue/hp-forum/src/views/home/pages/index/components/Subjects/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="home-index-subjects">
+        专题列表
+        <Row>
+            <Col :xs="24" :sm="8" v-for="item in 3" :key="item">
+                <div class="home-index-subjects-subject home-index-subjects-subject-hot">
+                    <div style="width:100%;height: 100px;background: cornflowerblue"></div>ColHot</div>
+            </Col>
+            <Col :xs="24" :sm="8" :lg="4" v-for="item in subjectList" :key="item">
+                <div class="home-index-subjects-subject">Col</div>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home-index-subjects",
+        data() {
+            return {
+                subjectList: 7
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .home-index-subjects {
+        padding: .5rem;
+
+        .home-index-subjects-subject {
+            padding: .5rem;
+            margin: .5rem;
+            border: 1px solid #eee;
+            border-radius: 6px;
+            cursor: pointer;
+            transition: all .3s ease-in-out;
+        }
+
+        .home-index-subjects-subject:hover {
+            box-shadow: 2px 2px 2px rgba(0, 0, 0, .1)
+
+
+        }
+
+        .home-index-subjects-subject-hot {
+
+        }
+    }
+</style>
