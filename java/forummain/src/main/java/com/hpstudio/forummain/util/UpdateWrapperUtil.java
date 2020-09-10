e17af52492094057ba6185351cf9ffe57de45f01
mawen <455766076@qq.com>
2020-09-10 13:40:43 +0800

    添加一个删除方法

--- java/forummain/src/main/java/com/hpstudio/forummain/util/UpdateWrapperUtil.java
+++ java/forummain/src/main/java/com/hpstudio/forummain/util/UpdateWrapperUtil.java
@@ -1,6 +1,7 @@
 package com.hpstudio.forummain.util;
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.hpstudio.forummain.exception.exceptionType.WrapperException;
 import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +16,18 @@ import java.lang.reflect.Field;
 public class UpdateWrapperUtil<T> {
     /**
      * 根据主键更新@Id
-     * @param t 对象
+     *
+     * @param t        对象
      * @param skipNull true：对空值不更新 false：将空值覆盖原有值
      * @return
      * @throws IllegalAccessException
      */
-    public UpdateWrapper<T> updateById(T t, boolean skipNull) throws IllegalAccessException {
+    public UpdateWrapper<T> updateWrapper(T t, boolean skipNull) throws IllegalAccessException {
         UpdateWrapper<T> wrapper = new UpdateWrapper<>();
-        Field[] fields = t.getClass().getDeclaredFields();
+        Class<?> aClass = t.getClass();
+        Field[] fields = aClass.getDeclaredFields();
+        String idField = null;
+        Object idVal = null;
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
             //获取字段原来权限
@@ -33,9 +38,9 @@ public class UpdateWrapperUtil<T> {
             String name = field.getName();
             //获取字段值
             Object val = field.get(t);
-            //
             if (null != field.getAnnotation(Id.class)) {
-                wrapper.eq(name, val);
+                idField = name;
+                idVal = val;
             } else {
                 if (skipNull && null == val) {
                     continue;
@@ -44,6 +49,44 @@ public class UpdateWrapperUtil<T> {
             }
             field.setAccessible(accessible);
         }
+        if (null == idField) {
+            throw new WrapperException(aClass.getName() + " 没有使用@Id标识主键");
+        }
+
+        if (null == idVal) {
+            throw new WrapperException(idField + " 主键的值为null");
+        }
+        wrapper.eq(idField, idVal);
+        return wrapper;
+    }
+
+    public UpdateWrapper<T> deleteWrapper(T t) throws NoSuchFieldException, IllegalAccessException {
+        UpdateWrapper<T> wrapper = new UpdateWrapper<>();
+        Class<?> aClass = t.getClass();
+        Field[] fields = aClass.getDeclaredFields();
+        String idField = null;
+        Object idVal = null;
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            boolean accessible = field.isAccessible();
+            field.setAccessible(true);
+            if (null != field.getAnnotation(Id.class)) {
+                idField = field.getName();
+                idVal = field.get(t);
+                field.setAccessible(accessible);
+                break;
+            }
+            field.setAccessible(accessible);
+        }
+        if (null == idField) {
+            throw new WrapperException(aClass.getName() + " 没有使用@Id标识主键");
+        }
+
+        if (null == idVal) {
+            throw new WrapperException(idField + " 主键的值为null");
+        }
+        wrapper.eq(idField, idVal);
+        wrapper.eq("isdel", 1);
         return wrapper;
     }
 
